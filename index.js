10b6857679bf1498cdd306c42dedc5146a360d5a
Gordon Martin <gordon.hugh.martin@gmail.com>
2017-08-15 22:16:54 +0100

    Sketch out the library interface.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// TODO: Ehh... The whole thing, really :3
+const pull = require('pull-stream');
+const Scroller = require('pull-scroll')
+
+// TODO: maist hings
+module.exports = (sbot, config) => {
+
+  // The root message that all the chat messages are linked back to
+  const rootMessageId = config.rootMessageId;
+
+  // The chat root message type to identify chat messages that are linked
+  // to the root and send me chat messages
+  const chatMessageType = config.chatMessageType;
+
+  /**
+   * Return the scroller HTML DOM element that the consuming code
+   * can attach to the DOM somewhere.
+   */
+  function getScrollerElement() {
+
+  }
+
+  /* Send the message using the configured message type and root message.
+   *  Additionally, link the message to each of the given ids.
+   */
+  function sendMessage(messageText, linkToMessageIds) {
+
+  }
+
+  return {
+    getScrollerElement: getScrollerElement,
+    sendMessage: sendMessage
+  }
+}
